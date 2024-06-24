776a249bfeecb679923338742b00c6ee739cf37b
wallace13 <wsalesg@hotmail.com>
2024-06-24 13:14:17 -0300

    Sitem

---
+++ src/main/java/sitem/sitem/SitemApplication.java
@@ -0,0 +1,13 @@
+package sitem.sitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sitem/sitem/SitemApplicationTests.java
@@ -0,0 +1,13 @@
+package sitem.sitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
