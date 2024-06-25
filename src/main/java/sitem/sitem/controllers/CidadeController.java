c9bfbbead627f795dbde9ce613360e8e70303534
wallace13 <wsalesg@hotmail.com>
2024-06-24 21:12:37 -0300

    Correção

--- src/main/java/sitem/sitem/controllers/CidadeController.java
+++ src/main/java/sitem/sitem/controllers/CidadeController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "Cidade")
+@RequestMapping(value = "cidade")
 public class CidadeController {
 
     private final CidadeService service;
